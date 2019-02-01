bb9ae3e2e65c0e077edd523d81c794fd4a764710
javierslzr <javiertifacient@gmail.com>
2019-01-31 21:38:12 -0600

    base project ready

---
+++ models/service.js
@@ -0,0 +1,7 @@
+module.exports = function (sequelize, DataTypes) {
+    var Servicios = sequelize.define("Servicios", {
+        servicio: DataTypes.STRING,
+        costo: DataTypes.INTEGER
+    });
+    return Servicios;
+};
